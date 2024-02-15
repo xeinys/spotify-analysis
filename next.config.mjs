67d3b888005e27276e5680f5209abeea73c404d9
xeinys <144363115+xeinys@users.noreply.github.com>
2024-02-15 13:23:38 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export'
+};
 
-export default nextConfig;
+module.exports = nextConfig;
